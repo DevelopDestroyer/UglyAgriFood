8af6f40ddc92ecb74a7a413ae3ff42ab304240f8
Jae Hong <o_oloveo@naver.com>
2020-11-12 20:57:46 +0900

    코드 이동

--- api/src/main/java/com/ugly/agri/ApiApplication.java
+++ api/src/main/java/com/ugly/agri/ApiApplication.java
@@ -1,56 +1,13 @@
 package com.ugly.agri;
 
-import com.ugly.agri.domain.RetailProduct;
-import com.ugly.agri.repository.RetailProductRepository;
 import lombok.RequiredArgsConstructor;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.IOException;
-
 @RequiredArgsConstructor
 @SpringBootApplication
-public class ApiApplication implements CommandLineRunner {
-    private final RetailProductRepository retailProductRepository;
-
+public class ApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws IOException {
-        crawling();
-    }
-
-    public void crawling() throws IOException {
-        String url = "http://www.kamis.or.kr/customer/price/retail/catalogue.do";
-        Document doc = Jsoup.connect(url).get();
-
-        Elements elements = doc.select("table#itemTable_1 tbody tr");
-
-        for (Element tr : elements) {
-            Elements tds = tr.select("td");
-            if (tds.size() > 0) {
-                if (tds.get(2).text().equals("상품")) {
-                    RetailProduct retailProduct = RetailProduct.builder()
-                            .name(tds.get(0).text())
-                            .kind(tds.get(1).text())
-                            .grade(tds.get(2).text())
-                            .todayAvgPrice(tds.get(3).text())
-                            .oneDayAvgPrice(tds.get(4).text())
-                            .oneWeekAvgPrice(tds.get(5).text())
-                            .twoWeekAvgPrice(tds.get(6).text())
-                            .oneMonthAvgPrice(tds.get(7).text())
-                            .oneYearAvgPrice(tds.get(8).text())
-                            .build();
-                    retailProductRepository.save(retailProduct);
-                }
-            }
-        }
     }
 }
--- api/src/main/java/com/ugly/agri/config/H2Configuration.java
+++ api/src/main/java/com/ugly/agri/config/H2Configuration.java
@@ -2,17 +2,20 @@ package com.ugly.agri.config;
 
 import com.google.common.collect.Lists;
 import com.ugly.agri.domain.Comment;
+import com.ugly.agri.domain.RetailProduct;
 import com.ugly.agri.domain.User;
-import com.ugly.agri.repository.CommentRepository;
-import com.ugly.agri.repository.OrderRepository;
-import com.ugly.agri.repository.ProductRepository;
-import com.ugly.agri.repository.UserRepository;
+import com.ugly.agri.repository.*;
 import lombok.RequiredArgsConstructor;
 import org.h2.tools.Server;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -20,6 +23,7 @@ import java.util.List;
 @Profile("local")
 @RequiredArgsConstructor
 public class H2Configuration {
+    private final RetailProductRepository retailProductRepository;
     private final UserRepository userRepository;
     private final OrderRepository orderRepository;
     private final CommentRepository commentRepository;
@@ -31,9 +35,37 @@ public class H2Configuration {
     }
 
     @Bean
-    public void setTestData() {
+    public void setTestData() throws IOException {
+        crawling();
         List<User> userList = userRepository.saveAll(getUserList());
         commentRepository.saveAll(getCommentList(userList));
+    }
+
+    public void crawling() throws IOException {
+        String url = "http://www.kamis.or.kr/customer/price/retail/catalogue.do";
+        Document doc = Jsoup.connect(url).get();
+
+        Elements elements = doc.select("table#itemTable_1 tbody tr");
+
+        for (Element tr : elements) {
+            Elements tds = tr.select("td");
+            if (tds.size() > 0) {
+                if (tds.get(2).text().equals("상품")) {
+                    RetailProduct retailProduct = RetailProduct.builder()
+                            .name(tds.get(0).text())
+                            .kind(tds.get(1).text())
+                            .grade(tds.get(2).text())
+                            .todayAvgPrice(tds.get(3).text())
+                            .oneDayAvgPrice(tds.get(4).text())
+                            .oneWeekAvgPrice(tds.get(5).text())
+                            .twoWeekAvgPrice(tds.get(6).text())
+                            .oneMonthAvgPrice(tds.get(7).text())
+                            .oneYearAvgPrice(tds.get(8).text())
+                            .build();
+                    retailProductRepository.save(retailProduct);
+                }
+            }
+        }
     }
 
     private List<User> getUserList() {
