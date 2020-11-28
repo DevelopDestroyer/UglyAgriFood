f656a37e4049d0b49c10928763091dca9ba6dcd7
Jae Hong <o_oloveo@naver.com>
2020-11-28 13:36:41 +0900

    product 검색 api 수정

--- api/src/main/java/com/ugly/agri/dto/SearchProductDTO.java
+++ api/src/main/java/com/ugly/agri/dto/SearchProductDTO.java
@@ -1,12 +1,16 @@
 package com.ugly.agri.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 
 @Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class SearchProductDTO {
     @NotBlank
     private String title;
